8e5e64cdc3117d109d5335ac8b0993c3c29b1ee9
Brian Carbone <brian@shufflrr.com>
2020-11-02 14:14:43 -0500

    project and logo configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'shufflrr-docs'
+project = 'Shufflrr'
 copyright = '2020, Shufflrr'
 author = 'Shufflrr'
 
@@ -53,3 +53,9 @@ html_theme = 'default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = 'img/logo.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
